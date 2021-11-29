04306e47185e75a670b53de671b894aadac6692d
thiagofelix1 <thiagofelixdasilva099@gmail.com>
2021-11-29 17:05:47 -0400

    stripe_integration

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,18 +1,25 @@
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import{signIn, signOut, useSession} from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-    const isUserLoggedIn = true;
-    return isUserLoggedIn ? (
-        <button className={styles.signInButton} type="button">
+
+    const[session] = useSession()
+
+    return session ? (
+        <button className={styles.signInButton} type="button"
+                onClick={() => signOut()}
+        >
         <FaGithub color="#04d361"/>
-        Thiago Felix
+        {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
     </button>
     ):(
-        <button className={styles.signInButton} type="button">
+        <button className={styles.signInButton} type="button"
+                onClick={() => signIn('github')}
+        >
             <FaGithub color="#eba417"/>
             Sign in with github
         </button>
